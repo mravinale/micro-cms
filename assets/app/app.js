38812ff9c454c2274620a19ba4be71ed2cd17afb
Mariano Ravinale <mravinale@gmail.com>
2013-12-10 19:23:24 -0300

    fixed litle error on load

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-define(['angular', 'angularRoute', 'bootstrap', 'angular-ui', 'angular-bootstrap-tpls', 'angular-strap', 'modernizr', 'angular-aloha'], function (angular) {
+define(['angular', 'bootstrap', 'angular-ui', 'angular-bootstrap-tpls', 'angular-strap', 'modernizr', 'angular-aloha'], function (angular) {
 
-    return angular.module('Heracles', ['ngRoute','ui', 'ui.bootstrap', '$strap.directives', 'aloha', 'compile', 'contenteditable','smoothscroll']);
+    return angular.module('Heracles', ['ui', 'ui.bootstrap', '$strap.directives', 'aloha', 'compile', 'contenteditable','smoothscroll']);
 });
 
